5d35002fd74fbb2f5a1ec8054b9d629963aad112
Tyler Alexander <spinnerbowl@hotmail.com>
2021-05-01 20:29:41 -0400

    Added base Impl for GtkWindow and clangd config

--- window/window.h
+++ window/window.h
@@ -3,14 +3,25 @@
 
 class(BGtkWindow)
 method_proto(BGtkWindow, void, SetTitle, char*);
+method_proto(BGtkWindow, void, Show);
 method_proto(BGtkWindow, void, Close);
 GtkWidget* window;
-method_proto(BGtkWindow, void, SignalConnect, char*, GCallback)
+method_proto(BGtkWindow, void, SignalConnect, char*, GCallback);
 class_end(BGtkWindow)
 
-BGtkWindow* BGtkWindow_init(int x, int y, char* title){
+BGtkWindow* BGtkWindow_init(int x, int y, char* title, GtkApplication* app){
 BGtkWindow* retval = new(BGtkWindow);
+retval->window = gtk_application_window_new(app);
 call_method(retval, SetTitle, title);
 gtk_window_set_default_size(GTK_WINDOW(retval->window), x, y);
-
+return retval;
+}
+method_def(BGtkWindow, void, SetTitle, char* title){
+      gtk_window_set_title(GTK_WINDOW(this->window), title);
+}
+method_def(BGtkWindow, void, Show){
+    gtk_widget_show_all(this->window);
+}
+method_def(BGtkWindow, void, Close){
+    gtk_widget_destroy(this->window);
 }
